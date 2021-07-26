e0c75084e4bb94e5f8c256919635c24bc4d6701c
sunK6 <kjs92627@gmail.com>
2021-07-26 20:05:09 +0900

    pf

--- Tomntoms_PJ/asset/js/common.js
+++ Tomntoms_PJ/asset/js/common.js
@@ -1,14 +1,45 @@
-
-
-$(function(){ /////// jQB /////////////////////////////
+$(function () { /////// jQB /////////////////////////////
 
     // .gnb>li 마우스 오버시 fadeIn/fadeOut
     $(".gnb>li").hover(
-        function(){ // over
-            $(".gnb_sub",this).stop().fadeIn(200);
+        function () { // over
+            $(".gnb_sub", this).stop().fadeIn(200);
         },
-        function(){ // out
-            $(".gnb_sub",this).stop().fadeOut(200);
+        function () { // out
+            $(".gnb_sub", this).stop().fadeOut(200);
         }); //////// hover ///////////
+
+    // .hamb 클릭시 .mobx와 .nav_bg에 class="on" 넣기, 
+    //              body에 overflowY:"hidden"
+    $(".hamb").click(function () {
+        $(".mobx").addClass("on");
+        $(".nav_bg").addClass("on");
+        $("body").css({
+            overflowY: "hidden"
+        });
+    }); ////////// click ///////////////
+
+    // .close클릭시 .mobx/.nav_bg class="on"빼기,
+    //              body에 overflowY:"auto"
+    $(".close").click(function () {
+        $(".mobx").removeClass("on");
+        $(".nav_bg").removeClass("on");
+        $("body").css({
+            overflowY: "auto"
+        });
+    }); //////// click ////////////
+
+    // 서브메뉴전체 숨기기  //
+    $(".mgnb_sub").hide();
+
+    // 상위 메뉴(.mgnb li) 클릭 시 하위메뉴 보이기(slideDown) //
+    $(".mgnb li").click(function () {
+        
+        $(".mgnb_sub").slideToggle(400)
+            .siblings().find(".mgnb_sub").slideUp(400);
+
+    }); ///////////// click ///////////
+
+
 
 }); ////////////// jQB /////////////////////////////////
